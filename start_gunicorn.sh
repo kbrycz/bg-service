0906ae1cd9e658ab8f948c89a980ab36d5120572
Karl Brycz <kbrycz@umich.edu>
2025-01-25 17:29:20 -0600

    add

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# "start_gunicorn.sh"
 cd /home/ec2-user/bg-service
 source venv/bin/activate
-exec gunicorn main:app -b 0.0.0.0:5000
+
+# Single worker, single thread, to reduce memory usage
+exec gunicorn main:app -b 0.0.0.0:5000 --workers=1 --threads=1
